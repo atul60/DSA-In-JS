e749d448ec10ad71ba0d0794ec9f9e21d8f13a59
unknown <satul388@gmail.com>
2025-04-29 00:30:42 +0530

    Maximum Average Subarray I

---
+++ SlidingWindow/findMaxAverage.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function (nums, k) {
+  let windowSum = 0;
+  for (let i = 0; i < k; i++) {
+    windowSum += nums[i];
+  }
+  let avg = windowSum / k;
+  let maxAvg = avg;
+  for (let i = k; i < nums.length; i++) {
+    windowSum += nums[i] - nums[i - k];
+    avg = windowSum / k;
+    maxAvg = Math.max(maxAvg, avg);
+  }
+
+  return maxAvg;
+};
